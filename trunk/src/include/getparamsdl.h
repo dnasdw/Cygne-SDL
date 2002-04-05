8f22eaeda2b1ed3a49dc4e33de9929acf73efd7c
nwagenaar <nwagenaar@720e92ca-6566-0410-b4a0-cee9537e9ff8>
2002-04-05 12:49:49 +0000

    SDL configfile support include
    git-svn-id: svn://svn.icculus.org/Cygne-SDL@7 720e92ca-6566-0410-b4a0-cee9537e9ff8

---
+++ trunk/src/include/getparamsdl.h
@@ -0,0 +1,50 @@
+#ifndef __GET_PARAM_H__
+#define __GET_PARAM_H__
+
+#include <stdio.h>
+
+int getparam(const char *file, const char *param);
+
+static char *keyparam[] = {
+	"cygne_up1",
+	"cygne_down1",
+	"cygne_left1",
+	"cygne_right1",
+	"cygne_up2",
+	"cygne_down2",
+	"cygne_left2",
+	"cygne_right2",
+	"cygne_start",
+	"cygne_a",
+	"cygne_b",
+	NULL
+};
+
+static char *joyparam[] = {
+	"cygne_joy_start",
+	"cygne_joy_a",
+	"cygne_joy_b",
+	NULL
+};
+
+enum { 
+	CYGNE_UP1,
+	CYGNE_DOWN1,
+	CYGNE_LEFT1,
+	CYGNE_RIGHT1,
+	CYGNE_UP2,
+	CYGNE_DOWN2,
+	CYGNE_LEFT2,
+	CYGNE_RIGHT2,
+	CYGNE_START,
+	CYGNE_A,
+	CYGNE_B
+};
+
+enum {
+	JOY_START,
+	JOY_A,
+	JOY_B
+};
+
+#endif
